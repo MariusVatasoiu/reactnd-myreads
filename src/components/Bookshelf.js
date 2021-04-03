fc095e5daafa507fd8f8fdbe73bd64057b664219
Marius Vatasoiu <marius.vatasoiu@outlook.com>
2021-04-03 20:51:50 +0300

    feat: convert bookshelf to functional component and add proptypes

--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -1,24 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
 import Book from "../components/Book.js";
+import PropTypes from "prop-types";
 
-class Bookshelf extends Component {
-  render() {
-    const { category, books, onUpdateBook } = this.props;
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{category.title}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {books.map((book) => (
-              <li key={book.id}>
-                <Book book={book} onUpdateBook={onUpdateBook} />
-              </li>
-            ))}
-          </ol>
-        </div>
+function Bookshelf({ category, books, onUpdateBook }) {
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{category.title}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {books.map((book) => (
+            <li key={book.id}>
+              <Book book={book} onUpdateBook={onUpdateBook} />
+            </li>
+          ))}
+        </ol>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+Bookshelf.propTypes = {
+  category: PropTypes.object.isRequired,
+  books: PropTypes.array.isRequired,
+  onUpdateBook: PropTypes.func.isRequired,
+};
 
 export default Bookshelf;
